2ef94b37efcc9bdd1173afd12cca9ec9bda089fa
apvolov <apvolov@gmail.com>
2026-02-16 15:36:40 +0300

    the branch react-spa-integration wad added

---
+++ spa/src/Routes.tsx
@@ -0,0 +1,10 @@
+export const ROUTES = {
+    HOME: '/',
+    SERVICES: '/principalities',
+    SERVICE_DETAIL: '/principalities/:id',
+};
+
+export const ROUTE_LABELS = {
+    HOME: 'Главная',
+    PRINCIPALITIES: 'Княжества',
+};
