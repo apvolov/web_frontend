fee68693a04d6acc7a8200d64d6214a3fd8b3e15
apvolov <apvolov@gmail.com>
2026-02-17 18:38:37 +0300

    some changes

--- redux_pwa/src/App.tsx
+++ redux_pwa/src/App.tsx
@@ -1,25 +1,11 @@
-import { useEffect } from 'react';
 import { HashRouter, Routes, Route, Link } from 'react-router-dom';
 import { Navbar } from 'react-bootstrap';
 import { HomePage, PrincipalitiesPage, PrincipalityPage } from "./pages";
 import { BreadCrumbs } from "./components/BreadCrumbs";
 import defaultMainLogo from "./assets/main_logo.png";
 import { ROUTES } from './Routes';
-import { invoke } from "@tauri-apps/api/core";
 
 function App() {
-  useEffect(() => {
-    invoke('tauri', { cmd: 'create' })
-      .then(() => { console.log("Tauri launched") })
-      .catch(() => { console.log("Tauri not launched (running in browser)") });
-
-    return () => {
-      invoke('tauri', { cmd: 'close' })
-        .then(() => { console.log("Tauri closed") })
-        .catch(() => { console.log("Error on close") });
-    };
-  }, []);
-
   return (
     <HashRouter>
       <Navbar bg="white" className="main-header shadow-none"> 
